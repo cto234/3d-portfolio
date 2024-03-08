1ab01a451b3aba6ecb1c6e5b7dcb463a8ae20f2c
cto234 <cto234@nyu.edu>
2024-03-08 14:57:30 -0500

    improved homepage

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -1,5 +1,6 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
+import './head.scss'
 
 function Model(props) {
   const { scene } = useGLTF("/head.glb");
@@ -14,7 +15,7 @@ function Model(props) {
 function Head() {
   return (
     <Canvas dpr={[1, 2]} camera={{ near: 0.1, far: 1000, fov: 45 }} style={{ position: "absolute" }}>
-      <color attach="background" args={["#fffff"]} />
+      <color attach="background" args={["#cfcccc"]} />
       <ambientLight intensity={1} castShadow={false}/>
       <PresentationControls speed={1.5} global zoom={0.5} polar={[0, 0]} >
         <Stage environment={"apartment"} intensity={0}>
