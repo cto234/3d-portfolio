3a57b28c070ebe6c42fe776769cc2fae4bd1d435
cto234 <cto234@nyu.edu>
2024-03-08 16:00:58 -0500

    Cleaned up homepage

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { Canvas, useFrame } from "@react-three/fiber";
 import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
 import './head.scss'
@@ -15,10 +16,10 @@ function Model(props) {
 function Head() {
   return (
     <Canvas dpr={[1, 2]} camera={{ near: 0.1, far: 1000, fov: 45 }} style={{ position: "absolute" }}>
-      <color attach="background" args={["#cfcccc"]} />
-      <ambientLight intensity={1} castShadow={false}/>
+      <color attach="background" args={["#e8e8e8"]} /> {/* #cfcccc */}
+      <ambientLight intensity={1} />
       <PresentationControls speed={1.5} global zoom={0.5} polar={[0, 0]} >
-        <Stage environment={"apartment"} intensity={0}>
+        <Stage environment={"apartment"} intensity={0} shadows={false}>
           <Model scale={0.007} />
         </Stage>
       </PresentationControls>
